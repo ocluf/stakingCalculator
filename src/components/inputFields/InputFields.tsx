68d141785c592b427beaf5582f9c8d293513dc68
o0x <o0x>
2021-02-28 19:51:01 +0000

    advanced input fields

--- src/components/inputFields/InputFields.tsx
+++ src/components/inputFields/InputFields.tsx
@@ -6,7 +6,6 @@ import ExpandMore from "@material-ui/icons/ExpandMore"
 import ExpandLess from "@material-ui/icons/ExpandLess"
 import InputAdornment from "@material-ui/core/InputAdornment"
 import PercentageSlider from "./PercentageSlider"
-import AdvancedSettings from "./AdvancedSettings"
 
 const InputFields = (props: { calculate: (x: CalculatorParameters) => void }) => {
   // standard fields
@@ -42,8 +41,21 @@ const InputFields = (props: { calculate: (x: CalculatorParameters) => void }) =>
     })
   }
 
+  const votingPercMarks = [
+    { value: 25,  label: '25%' },
+    { value: 50,  label: '50%' },
+    { value: 75,  label: '75%' },
+    { value: 100, label: '100%'}
+  ]
+
   return (
-    <form className="flex flex-shrink-0 flex-col space-y-4 w-96 m-5">
+    <form className="flex flex-shrink-0 flex-col space-y-5 w-96 m-5">
+
+      {/* Simple options */}
+
+      {/* stake size */}
+      {/* <div className="">If you deposit this many ICP tokens in your neuron:</div> */}
+      <div className="">The graph shows the potential returns you could expect if you deposit this many ICP tokens in your neuron:</div>
       <TextField
         required={true}
         id="ICP_Amount"
@@ -58,76 +70,136 @@ const InputFields = (props: { calculate: (x: CalculatorParameters) => void }) =>
         onChange={e => setStake(e.target.value)}
         aria-labelledby="stake-size"
       ></TextField>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="Starting date"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            "aria-label": "change date",
-          }}
+
+      {/* starting date */}
+      <div>
+        <div className="">On this date:</div>
+        <div className="flex flex-shrink-0 flex-col">
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="starting date"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </MuiPickersUtilsProvider>
+        </div>
+      </div>
+
+      {/* dissolve period */}
+      <div className="flex">
+        <span className="flex-1">With a dissolve period of:</span>
+        <span className="flex">{lockupPeriod} year{lockupPeriod!=1?"s":""}</span>
+      </div>
+      <div>
+        <Slider 
+          id="lockup_period"
+          defaultValue={5}
+          min={0}
+          max={8}
+          step={0.1}
+          value={lockupPeriod}
+          onChange={ (e, value) => setPeriod(value)} 
+          aria-labelledby="lockup-period"
         />
-      </MuiPickersUtilsProvider>
-      <PercentageSlider
-        title={"With a dissolve period of:"}
-        percentage={lockupPeriod}
-        setPerc={setPeriod}
-        postfix={lockupPeriod > 1 ? "years" : "year"}
-        max={8}
-        step={0.1}
-        defaulValue={5}
-      />
-      <AdvancedSettings open={open} handleExpand={handleExpand}>
-        <PercentageSlider
-          title="percentage of votes participated in:"
+      </div>
+
+      <div className="">
+        Neurons on the Internet Computer are active participants in the network's governance system, 
+        with votes placed on proposals or delegated to others. The more you participate, the higher your rewards.
+      </div>
+      <div>
+        Vote participation:
+      </div>
+      <div>
+        {/* <PercentageSlider
+          title="Vote participation:"
           percentage={votingPerc}
           setPerc={setVotingPerc}
           postfix="%"
           max={100}
           step={1}
+          marks={votingPercMarks}
           defaulValue={100}
-        />
-        <PercentageSlider
-          title="percentage of total supply staked:"
-          percentage={stakePerc}
-          setPerc={setStakePerc}
-          postfix="%"
-          defaulValue={90}
+        /> */}
+        <Slider 
+          id="voting percentage"
+          defaultValue={75}
+          min={0}
           max={100}
           step={1}
+          value={votingPerc}
+          marks={votingPercMarks}
+          onChange={ (e, value) => setVotingPerc(value)} 
+          aria-labelledby="voting-percentage"
         />
-        <div>for now assumed constant total supply:</div>
-        <TextField
-          required={true}
-          id="total supply"
-          label="total supply"
-          value={totalSupply}
-          variant="outlined"
-          placeholder="total number of tokens"
-          type="number"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">ICP</InputAdornment>,
-          }}
-          onChange={e => setTotalSupply(e.target.value)}
-          aria-labelledby="stake-size"
-        ></TextField>
-      </AdvancedSettings>
-      {/** 
-        <div className="flex flex-col space-y-3 pb-11">
-          <div className="flex justify-center text-xl font-light">You can earn a daily return of up to</div>
-          <div className="flex justify-center text-2xl">Some tokens</div>
-        </div>
+      </div>
 
-        <div className="">
-          Neurons on the Internet Computer are active participants in the network's governance system, with votes placed
-          on proposals or delegated to others. The more you participate, the higher your rewards.
+      {/* Advanced options */}
+
+      <div className="flex justify-end w-full mb-4 text-blue-500 cursor-pointer" onClick={() => handleExpand()}>
+        Advanced settings
+        <div className="ml-2">{open ? <ExpandLess /> : <ExpandMore />}</div>
+      </div>
+
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <div className="flex flex-col w-full space-y-4">
+          <div className="text-sm mb-2">
+            These settings are for advanced users interested in exploring the effect of global network parameters. 
+            They are not part of your neuron's settings.
+          </div>
+
+          {/* total ICP supply */}
+          <div>
+            <div>Total token supply:</div>
+            <div className="text-sm">
+              The global token supply is determined by the network as a whole, but you can explore the effect of changing 
+              the total supply by adjusting this parameter. 
+            </div>
+          </div>
+          <TextField
+            required={true}
+            id="total supply"
+            label="total supply"
+            value={totalSupply}
+            variant="outlined"
+            placeholder="total number of tokens"
+            type="number"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">ICP</InputAdornment>,
+            }}
+            onChange={e => setTotalSupply(e.target.value)}
+            aria-labelledby="stake-size"
+          ></TextField>
+          <sup className="text-gray-400">* assumed constant total supply over time, for now</sup>
+
+          {/* % of total supply staked in neurons */}
+          <div className="flex flex-col">
+            <div>Percentage of total supply staked in neurons:</div>
+            <div className="text-sm">
+              Fewer participants mean higher rewards.            
+            </div>
+          </div>
+          <PercentageSlider
+            title="% of network tokens staked in neurons:"
+            percentage={stakePerc}
+            setPerc={setStakePerc}
+            postfix="%"
+            defaulValue={90}
+            max={100}
+            step={1}
+          />
+
         </div>
-        */}
+      </Collapse>
+
       <Button variant="contained" color="primary" onClick={handleCalculate}>
         Calculate
       </Button>
